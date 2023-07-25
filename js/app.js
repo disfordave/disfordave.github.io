beda18edbfa6f6450871482961b1a9d25b8481cc
reikavik <118544840+reikavik@users.noreply.github.com>
2023-07-25 20:41:54 +0900

    Add french site

--- js/app.js
+++ js/app.js
@@ -1,16 +1,16 @@
 
-function ClickMore() {
+document.getElementById("more-button").addEventListener("click", function() {
     document.getElementById("more-button").style.display = "none";
     document.getElementById("less-button").style.display = "inline";
     document.getElementById("hidden").style.display = "inline";
     document.getElementById("hidden-2").style.display = "inline";
     document.getElementById("hidden-dots").style.display = "none";
-};
+});
 
-function ClickLess() {
+document.getElementById("less-button").addEventListener("click", function() {
     document.getElementById("less-button").style.display = "none";
     document.getElementById("more-button").style.display = "inline";
     document.getElementById("hidden").style.display = "none";
     document.getElementById("hidden-2").style.display = "none";
     document.getElementById("hidden-dots").style.display = "inline";
-}
+});
